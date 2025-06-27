e0426e2fa8bdc75a1514a2d89726f54dddc8095a
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-27 12:49:45 +0530

    Implimented authentication feature using next auth / github intigration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      Home
+      <h1 className="text-2xl">Home</h1>
     </>
   );
 }
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import { auth, signIn, signOut } from '@/auth'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = async () => {
+    const session = await auth()
+    return (
+        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className='flex justify-between items-center'>
+                <Link href="/">
+                    <img src="/logo.png" alt="Logo" width={144} height={30} />
+                </Link>
+                <div className='flex items-center gap-5 text-black'>
+                    {session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span>Create</span>
+                            </Link>
+                            <form action={async () => {
+                                "use server"
+                                await signOut()
+                            }}>
+                                <button type='submit'>Logout</button>
+                            </form>
+                            <Link href={`/user/${session?.user?.id}`}>
+                                <span>{session?.user?.name}</span>
+                            </Link>
+                        </>
+                    ) : (
+                        <form action={async () => {
+                            "use server"
+                            await signIn()
+                        }}>
+                            <button type='submit'>Login</button>
+                        </form>
+                    )}
+                </div>
+            </nav>
+        </header >
+    )
+}
+
+export default Navbar
