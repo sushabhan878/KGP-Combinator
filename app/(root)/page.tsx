7f5cf616df5b5ba5892c3a182204278f2b9d610b
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-28 00:10:14 +0530

    Added hero banner

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="heading">Home</h1>
+      <section className="w-full bg-primary min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
+        <h1 className="uppercase bg-black px-6 py-3 font-workSans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5;
+">Pitch Your Startup, <br />Connect with Entrepreneurs</h1>
+        <p className="font-medium mt-4 text-[20px] text-white max-w-3xl text-center break-words">Submit Ideas, Vote on Pitches, And Get Noticed in Virtual Comprtions.</p>
+      </section>
     </>
   );
 }
