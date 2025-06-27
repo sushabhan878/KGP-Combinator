ece9a71eb282a641be78808fe56032e9a62d515e
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-27 13:36:24 +0530

    Implimented Custom font and changed globals.css along with root layout.tsx also implimented tailwind.config.css

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Navbar = async () => {
     const session = await auth()
     return (
-        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+        <header className='px-5 py-3 bg-white shadow-sm font-semibold'>
             <nav className='flex justify-between items-center'>
                 <Link href="/">
                     <img src="/logo.png" alt="Logo" width={144} height={30} />
