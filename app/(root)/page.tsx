f8bdbe13b11dab7b6b0dc2f49a0b6631f38160b4
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-28 12:30:25 +0530

    Implimented startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,21 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
-  const query = (await searchParams).query
+  const query = (await searchParams).query;
+  const posts = [{
+    _createdAt: Date.now(),
+    views: 55,
+    author: {
+      _id: 1,
+      name: "Elon Musk",
+    },
+    _id: 1,
+    description: "First startup post",
+    image: "https://tse4.mm.bing.net/th/id/OIP.UUD5ia8fx--kBVTt-Qu81wHaE8?rs=1&pid=ImgDetMain",
+    category: "Robots",
+    title: "We Robot"
+  }]
   return (
     <>
       <section className="w-full bg-primary min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
@@ -9,6 +23,20 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
 ">Pitch Your Startup, <br />Connect with Entrepreneurs</h1>
         <p className="font-medium mt-4 text-[20px] text-white max-w-3xl text-center break-words">Submit Ideas, Vote on Pitches, And Get Noticed in Virtual Comprtions.</p>
         <SearchForm query={query} />
+      </section>
+      <section className="px-6 py-10 max-w-7xl mx-auto">
+        <p className="font-workSans font-semibold text-[30px] text-black">
+          {query ? `Search Results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 grid md:grid-cols-3 sm:grid-cols-2 gap-5">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="text-black-100 text-sm font-normal">No Startup Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
---
+++ components/StartupCard.tsx
@@ -0,0 +1,50 @@
+import { formatDate } from '@/lib/utils'
+import { EyeIcon } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+
+const StartupCard = ({ post }: { post: StartupCardType }) => {
+    return (
+        <li className='bg-white border-[5px] border-black py-6 px-5 rounded-[22px] shadow-200 hover:border-pink-400 transition-all duration-500 hover:shadow-yellow-100 hover:bg-pink-50 group'>
+            <div className='flex justify-between items-center'>
+                <p className='font-workSans font-medium text-[16px] bg-yellow-200 px-4 py-2 rounded-full group-hover:bg-white-100'>
+                    {formatDate(post._createdAt)}
+                </p>
+                <div className='flex gap-1.5'>
+                    <EyeIcon className='size-6 text-pink-500' />
+                    <span className='font-workSans font-medium text-[16px] text-black'>{post.views}</span>
+                </div>
+            </div>
+            <div className='flex justify-between items-center mt-5 gap-5'>
+                <div className='gap-1'>
+                    <Link href={`/user/${post.author?._id}`}>
+                        <p className='font-workSans font-medium text-[16px] text-black line-clamp-1'>{post.author?.name}</p>
+                    </Link>
+                    <Link href={`/startup/${post._id}`}>
+                        <h3 className='font-workSans font-semibold text-[26px] text-black line-clamp-1'>{post.title}</h3>
+                    </Link>
+                </div>
+                <Link href={`/user/${post.author?._id}`}>
+                    <img src="https://placehold.co/48x48" alt="Placeholder" height={48} width={48} className='rounded-full' />
+                </Link>
+            </div>
+            <Link href={`/startup/${post._id}`}>
+                <p className='font-workSans font-normal text-[16px] line-clamp-2 my-3 text-black-100 break-all'>{post.description}</p>
+                <img src={post.image} alt="Placeholder" className='w-full h-[164px] rounded-[10px] object-cover' />
+            </Link>
+            <div className='flex justify-between items-center gap-3 mt-5'>
+                <Link href={`/?query=${post.category.toLowerCase()}`}>
+                    <p className='font-workSans font-medium text-[16px] text-black'>{post.category}</p>
+                </Link>
+                <Button className='rounded-full bg-gray-900 font-medium text-[16px] text-white px-5 py-3' asChild>
+                    <Link href={`/startup/${post._id}`}>
+                        Details
+                    </Link>
+                </Button>
+            </div>
+        </li>
+    )
+}
+
+export default StartupCard
