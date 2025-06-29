87769f75b727aa3d8db08ab26374b7922bb2514c
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-29 21:27:41 +0530

    Implimented live content sharing using incrimental static regeneration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,13 @@
 import StartupCard, { StartupCardType } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
-import { client } from "@/sanity/lib/client";
 import { STARTUP_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
-
-  const posts = await client.fetch(STARTUP_QUERY)
-  // const posts = [{
-  //   _createdAt: Date.now(),
-  //   views: 55,
-  //   author: {
-  //     _id: 1,
-  //     name: "Elon Musk",
-  //   },
-  //   _id: 1,
-  //   description: "First startup post",
-  //   image: "https://tse4.mm.bing.net/th/id/OIP.UUD5ia8fx--kBVTt-Qu81wHaE8?rs=1&pid=ImgDetMain",
-  //   category: "Robots",
-  //   title: "We Robot"
-  // }]
+  const { data: posts } = await sanityFetch({
+    query: STARTUP_QUERY
+  })
   return (
     <>
       <section className="w-full bg-primary min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
@@ -42,6 +30,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
