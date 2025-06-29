ca086c68052ebd369f6d05c6d45418acf0642508
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-29 22:39:42 +0530

    Getting data for startup details page ..

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -12,6 +12,23 @@ export const STARTUP_QUERY = defineQuery(
         viewa,
         description,
         category,
+        image
+    }`
+)
+
+
+export const STARTUP_BY_ID_QUERY = defineQuery(
+    `* [_type == "startup" && _id == $id][0] {
+        _id,
+        title,
+        slug,
+        _createdAt,
+        author -> {
+            _id, name, username, image, bio
+        },
+        viewa,
+        description,
+        category,
         pitch,
         image
     }`
