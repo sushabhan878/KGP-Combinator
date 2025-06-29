cc7be82b8d9e70264bd0becff6549d0e1b4b73aa
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-29 21:42:57 +0530

    Implimented some changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,12 +5,14 @@ import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
+  const params = { search: query || null }
   const { data: posts } = await sanityFetch({
-    query: STARTUP_QUERY
+    query: STARTUP_QUERY,
+    params
   })
   return (
     <>
-      <section className="w-full bg-primary min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
+      <section className="w-full bg-primary min-h-[530px] flex pattern justify-center items-center flex-col py-10 px-6">
         <h1 className="uppercase bg-black px-6 py-3 font-workSans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5;
 ">Pitch Your Startup, <br />Connect with Entrepreneurs</h1>
         <p className="font-medium mt-4 text-[20px] text-white max-w-3xl text-center break-words">Submit Ideas, Vote on Pitches, And Get Noticed in Virtual Comprtions.</p>
