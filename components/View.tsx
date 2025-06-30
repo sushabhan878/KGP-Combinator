cc64fa81bb382c7ff60f8a738625b8cb88bf08d9
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-30 14:00:06 +0530

    Implimented the live view update feature on each relode. and optimise some features in client.ts fiel

--- components/View.tsx
+++ components/View.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Ping from './Ping'
 import { client } from '@/sanity/lib/client'
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import { writeClient } from '@/sanity/lib/write-client'
+import { after } from "next/server"
 
 const View = async ({ id }: { id: string }) => {
     const { viewa: totalViews } = await client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, { id })
     
-
+    after(async () => await writeClient.patch(id).set({ viewa: totalViews + 1}).commit())
     // TODO: Update number of views when someone view the post
   return (
     <div className='flex justify-end items-center mt-5 fixed bottom-3 right-3'>
