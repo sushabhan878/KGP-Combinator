8eda7b4772e26113b104e6e2aa6cfc6284adc459
Sushabhan <sushabhanmajumder878@gmail.com>
2025-06-30 22:40:27 +0530

    Paused for now .. I will work on it some times latter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,13 @@ import StartupCard, { StartupCardType } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 import { STARTUP_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+// import { auth } from "@/auth";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
   const params = { search: query || null }
+  // const session = await auth()
+  // console.log(session?.id)
   const { data: posts } = await sanityFetch({
     query: STARTUP_QUERY,
     params
